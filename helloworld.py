f6c85f0a8361de5c47d6593316906a49607d1288
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-01-24 21:54:07 -0500

    Add files via upload
    Signed-off-by: Ameya172 <123523953+Ameya172@users.noreply.github.com>

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import datetime
+
+def my_brand():
+    print("=*=*=*= Ameya Sawant =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567 =*=*=*=")
+    print("=*=*=*= HW_00 =*=*=*=")
+    print("=*=*=*= Date and Time: " + str(datetime.datetime.now()) + " =*=*=*=")
+    
+my_brand()
+print("Hello World !")
+
